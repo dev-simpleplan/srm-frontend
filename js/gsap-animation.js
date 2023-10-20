526852e65d4cace8fba1b851089e928c13a6d041
dev-simpleplan <simple.website.testing@gmail.com>
2023-10-20 13:04:56 +0530

    Update code

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -1,33 +1,87 @@
+gsap.registerPlugin(ScrollTrigger);
+let mm3 = gsap.matchMedia();
+mm3.add("(min-width: 1300px)", () => {
+ScrollTrigger.defaults({
+  markers: false
+});
+const tl = gsap.timeline({
+  scrollTrigger: {
+  trigger: ".featured-video-section",
+    
+    start: "-20% center",
+    end: "+=300",
+    scrub: 1,
+    pin: false
+  }
+});
+tl
+.from(".featured-slider-wrap", {
+    scale: 0.2,
+    ease: "none" // No ease feels better with scrubed values
+  })
+.to(".featured-slider-wrap", {
+  scale: 1,
+  ease: "none" // No ease feels better with scrubed values
+});
+});
 
-// gsap.registerPlugin(ScrollTrigger);
 
-// const tl = gsap
-//   .timeline({
-//     scrollTrigger: {
-//       trigger: ".parallax-banner",
-//       markers: false,
-//       start: "10% 19%",
-//       end: "180% bottom",
-//       pin: true,
-//       scrub: 1,
-//       onUpdate: (self) => {
-//         const progress = self.progress;
-//         if (progress === 0) {} else if (progress <= 0.50) {
-//           gsap.set('.parallax-slide-image', {position: 'fixed'});
-//         } else if (progress <= 1) {} else {};
-//       }
-//     }
-//   })
-//   tl
-//   .to(".parallax-slide-content", {
-//     transform: 'translate3d(0,-100vh,0)'
-//   }, 0)
-//   .to(".parallax-slides-wrap .parallax-slide:nth-child(2) .parallax-slide-image", {
-//     height: '100vh'
-//   }, 0)
-//   .to(".parallax-slide-content", {
-//     transform: 'translate3d(0,-200vh,0)'
-//   }, 0.5)
-//   .to(".parallax-slides-wrap .parallax-slide:nth-child(3) .parallax-slide-image", {
-//     height: '100vh'
-//   }, 0.5)
+// Banner  Content sticky GSAP
+gsap.registerPlugin(ScrollTrigger);
+let mm = gsap.matchMedia();
+mm.add("(min-width: 1300px)", () => {
+ScrollTrigger.defaults({
+  markers: false
+});
+
+
+const t2 = gsap.timeline({
+  scrollTrigger: {
+  trigger: ".banner-content-wrapper ",
+    
+    start: "10% 30%",
+    end: "+=300",
+    scrub: 1,
+    pin: true
+  }
+});
+t2
+.from(".banner-content-box", {
+    y: "0%",
+    ease: "none" // No ease feels better with scrubed values
+  })
+.to(".banner-content-box", {
+  y: "30",
+  ease: "none" // No ease feels better with scrubed values
+});
+});
+
+
+// Banner Image sticky GSAP
+
+gsap.registerPlugin(ScrollTrigger);
+let mm2 = gsap.matchMedia();
+mm2.add("(min-width: 1300px)", () => {
+ScrollTrigger.defaults({
+  markers: false
+});
+const t3 = gsap.timeline({
+  scrollTrigger: {
+  trigger: ".banner-img-wrapper",
+    
+  start: "20% 20%",
+  end: "60% 100%",
+  scrub: 1,
+  pin: true
+  }
+});
+t3
+.from(".banner-img ", {
+    opacity: 0.9,
+    ease: "none" // No ease feels better with scrubed values
+  })
+.to(".banner-img ", {
+  opacity: 0.2,
+  ease: "none" // No ease feels better with scrubed values
+});
+});
